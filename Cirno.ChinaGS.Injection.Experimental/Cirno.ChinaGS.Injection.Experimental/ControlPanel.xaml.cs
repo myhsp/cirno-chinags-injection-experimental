24a538dbaaa9c993e87defec6256535780f613fe
konata233 <52315359+konata233@users.noreply.github.com>
2024-02-05 16:44:28 +0800

    Add files via upload

---
+++ Cirno.ChinaGS.Injection.Experimental/Cirno.ChinaGS.Injection.Experimental/ControlPanel.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace Cirno.ChinaGS.Injection.Experimental
+{
+    /// <summary>
+    /// ControlPanel.xaml 的交互逻辑
+    /// </summary>
+    public partial class ControlPanel : UserControl
+    {
+        public Program program;
+
+        public ControlPanel()
+        {
+            InitializeComponent();
+        }
+
+        public ControlPanel(Program prog)
+        {
+            InitializeComponent();
+            this.program = prog;
+        }
+
+        private void Btn_1_Click(object sender, RoutedEventArgs e)
+        {
+            Process[] processesByName = Process.GetProcessesByName("LockMouse");
+            if (processesByName != null)
+            {
+                Process[] array = processesByName;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i].Kill();
+                }
+            }
+
+            ShowWindow(FindWindow("progman", null), 0);
+            ShowWindow(FindWindow("Shell_traywnd", null), 0);
+            ShowWindow(FindWindow("button", null), 0);
+
+            ShowWindow(FindWindow("progman", null), 1);
+            ShowWindow(FindWindow("Shell_traywnd", null), 1);
+            ShowWindow(FindWindow("button", null), 1);
+        }
+
+        private void Btn_2_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("osk.exe");
+        }
+
+        private void Btn_3_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.program.RemoveAdminPanel();
+                this.program.isPanelOpened = false;
+            }
+            catch
+            {
+                StatusLabel.Content = "错误：无法关闭窗口";
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern int ShowWindow(IntPtr hwnd, int nStyle);
+    }
+}
---
+++ Cirno.ChinaGS.Injection.Experimental/Cirno.ChinaGS.Injection.Experimental/Program.cs
@@ -0,0 +1,124 @@
+﻿using GS.Unitive.Framework.Core;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Cirno.ChinaGS.Injection.Experimental
+{
+    public class Program : IAddonActivator
+    {
+        public IAddonContext addonContext;
+        public ControlPanel panel;
+        public string panelGuid;
+        public bool isPanelOpened;
+
+        public bool debugging = false;
+
+        public void Start(IAddonContext context)
+        {
+            debugging = true;
+            isPanelOpened = false;
+            this.addonContext = context;
+            this.panel = new ControlPanel(this);
+
+            RegistCardCallBack();
+        }
+
+        public void Stop(IAddonContext context)
+        {
+            
+        }
+
+        public void DisplayAdminPanel()
+        {
+            panelGuid = AddGarnitureControl(panel, 560, 180);
+        }
+
+        public void RemoveAdminPanel()
+        {
+            RemoveGarnitureControl(panelGuid);
+        }
+
+        public void RegistCardCallBack()
+        {
+            string cards = this.addonContext.DictionaryValue("baseConfig", "cards");
+
+            dynamic deviceMan = this.addonContext.GetFirstOrDefaultService("GS.Terminal.DeviceManager",
+                "GS.Terminal.DeviceManager.Service.DeviceCallControl");
+
+            deviceMan.RegistCardCallback(
+                    new Action<dynamic>(
+                        delegate (dynamic data)
+                        {
+                            string cardId = "ABCDEFFF";
+                            cardId = data.OperaDeviceData.Message;
+
+                            string hashString = ComputeHash(cardId);
+
+                            if (debugging)
+                            {
+                                this.addonContext.Logger.Info("Experiment::" + hashString);
+                            }
+
+                            if (cards.Contains(hashString) || cards.Contains(cardId))
+                            {
+                                if (!isPanelOpened)
+                                {
+                                    this.DisplayAdminPanel();
+                                }
+                                else
+                                {
+                                    this.RemoveAdminPanel();
+                                }
+                                isPanelOpened = !isPanelOpened;
+                            }
+                        }
+                    )
+                );
+        }
+
+        public string AddGarnitureControl(UserControl control, double left, double top)
+        {
+            ///<summary>
+            /// 添加用户控件
+            ///<returns></returns>
+            ///</summary>
+            dynamic uiService = this.addonContext.GetFirstOrDefaultService("GS.Terminal.MainShell",
+                "GS.Terminal.MainShell.Services.UIService");
+
+            string guid = uiService.AddGarnitureControl(control, top, left);
+            return guid;
+        }
+
+        public void RemoveGarnitureControl(string guid)
+        {
+            ///<summary>
+            /// 移除用户控件
+            ///<returns></returns>
+            ///</summary>
+            dynamic uiService = this.addonContext.GetFirstOrDefaultService("GS.Terminal.MainShell",
+                "GS.Terminal.MainShell.Services.UIService");
+
+            uiService.RemoveGarnitureControl(guid);
+        }
+
+        public string ComputeHash(string source)
+        {
+            string hashString;
+            try
+            {
+                using (SHA256 sha256 = SHA256.Create())
+                {
+                    byte[] hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(source));
+                    hashString = BitConverter.ToString(hash).Replace("-", "").ToLower();
+                }
+            }
+            catch
+            {
+                hashString = "error";
+            }
+            return hashString;
+        }
+    }
+}
